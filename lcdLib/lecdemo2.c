3ec7f366728ccf759bfc4f0804d5eef0d781d896
Vivian Sanchez <vsanchez17@miners.utep.edu>
2021-04-06 10:21:14 -0600

    Trying again

---
+++ lcdLib/lecdemo2.c
@@ -0,0 +1,37 @@
+/** \file lcddemo.c
+ *  \brief A simple demo that draws a string and square
+ */
+
+#include <libTimer.h>
+#include "lcdutils.h"
+#include "lcddraw.h"
+
+/** Initializes everything, clears the screen, draws "hello" and a square */
+int
+main()
+{
+  configureClocks();
+  lcd_init();
+  u_char width = screenWidth, height = screenHeight;
+
+  clearScreen(COLOR_BLUE);
+
+  drawString5x7(20,20, "hello", COLOR_GREEN, COLOR_RED);
+
+  // fillRectangle(30,30, 60, 60, COLOR_ORANGE);
+  u_char offset_r = 50, offset_c = 50;
+  for (int r = 0; r <= 50; r++){
+    for (int c = 0; c <= r; c++){
+      drawPixel(offset_c + c, offset_r + r, COLOR_ORANGE);
+    }
+  }
+
+  // for (int r = 0; r >= 10; r++){
+  // for (int c = 0; c <= 10; c++){
+      
+  //  drawPixel(offset_c + c, offset_r + r, COLOR_ORANGE);
+  //  drawPixel(offset_c - c, offset_r + r, COLOR_ORANGE);
+
+  // }
+  // }
+}
